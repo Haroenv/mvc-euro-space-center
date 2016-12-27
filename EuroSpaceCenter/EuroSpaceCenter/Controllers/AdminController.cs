c6a45f8d21847c2f20762725927be8bec6b0371c
Haroen Viaene <haroen.viaene@student.odisee.be>
2016-12-27 20:52:26 +0100

    stom

---
+++ EuroSpaceCenter/EuroSpaceCenter/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using EuroSpaceCenter.Models;
+using EuroSpaceCenter.util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EuroSpaceCenter.Controllers {
+    [CustomAuthorizeAttribute(Roles = "admin")]
+    public class AdminController : Controller {
+        // GET: Admin
+        public ActionResult Index() {
+            ViewBag.Restaurants = items.Get("restaurants");
+            ViewBag.Attractions = items.Get("attractions");
+            ViewBag.Shows = items.Get("shows");
+            return View();
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: Admin/create
+        [HttpPost]
+        public ActionResult Create(item i) {
+            return View();
+        }
+    }
+}
