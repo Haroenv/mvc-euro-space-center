0a0d13be6dd61b5282eb5719f7b3df1162b5b231
Haroen Viaene <haroen.viaene@student.odisee.be>
2016-12-28 20:42:40 +0100

    zever

---
+++ EuroSpaceCenter/EuroSpaceCenter/Controllers/PlanController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EuroSpaceCenter.Controllers {
+    [Authorize]
+    public class PlanController : Controller {
+        // GET: Plan
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult Join() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(int id) {
+            return View();
+        }
+    }
+}
