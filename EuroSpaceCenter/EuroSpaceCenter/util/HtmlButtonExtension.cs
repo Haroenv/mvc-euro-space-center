2d829674703648f65e728b1c7d264d39d8b3f9dd
Haroen Viaene <haroen.viaene@student.odisee.be>
2016-12-28 12:23:07 +0100

    knop

---
+++ EuroSpaceCenter/EuroSpaceCenter/util/HtmlButtonExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+public static class HtmlButtonExtension {
+
+    public static MvcHtmlString Button(this HtmlHelper helper,
+                                       string innerHtml,
+                                       object htmlAttributes) {
+        return Button(helper, innerHtml,
+                      HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes)
+        );
+    }
+
+    public static MvcHtmlString Button(this HtmlHelper helper,
+                                       string innerHtml,
+                                       IDictionary<string, object> htmlAttributes) {
+        var builder = new TagBuilder("button");
+        builder.InnerHtml = innerHtml;
+        builder.MergeAttributes(htmlAttributes);
+        return MvcHtmlString.Create(builder.ToString());
+    }
+}
