cd66b2e907a2394937cfc9fa425497a0b91328fc
Haroen Viaene <haroen.viaene@student.odisee.be>
2016-12-29 17:31:42 +0100

    fix exenderino

---
+++ EuroSpaceCenter/EuroSpaceCenter/Models/activation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EuroSpaceCenter.Models {
+    public partial class activation {
+        public static activation create(user u) {
+            using (var db = new DataClassesDataContext()) {
+                try {
+                    var a = new activation();
+                    a.code = util.Rand.String(15);
+                    a.users_id = u.id;
+                    db.activations.InsertOnSubmit(a);
+                    db.SubmitChanges();
+                    return a;
+                } catch (Exception e) {
+                    throw e;
+                }
+            }
+        }
+
+        public static bool activate(string code) {
+            using (var db = new DataClassesDataContext()) {
+                try {
+                    var act = db.activations.SingleOrDefault(ac => ac.code == code);
+                    if (act != null) {
+                        act.user.verified = true;
+                        db.activations.DeleteOnSubmit(act);
+                        db.SubmitChanges();
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } catch (Exception e) {
+                    throw e;
+                }
+            }
+        }
+    }
+}
---
+++ EuroSpaceCenter/EuroSpaceCenter/Models/user.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace EuroSpaceCenter.Models {
+    public partial class user {
+        internal static bool Create(user u) {
+            using (var db = new DataClassesDataContext()) {
+                try {
+                    u.password = util.Encryption.Hash.CreateHash(u.password);
+                    db.users.InsertOnSubmit(u);
+                    db.SubmitChanges();
+                    var a = activations.create(u);
+                    if (a == null) {
+                        return false;
+                    }
+                    return util.Email.sendVerification(u, a);
+                } catch (Exception e) {
+                    throw e;
+                }
+            }
+        }
+
+        internal static void Update(user old, user newUser) {
+            using (var db = new DataClassesDataContext()) {
+                user usr = db.users.SingleOrDefault(us => us.email == old.email);
+                usr.email = newUser.email;
+                usr.name = newUser.name;
+                db.SubmitChanges();
+            }
+        }
+
+        internal static void SetPassword(string email, string password) {
+            using (var db = new DataClassesDataContext()) {
+                user u = db.users.SingleOrDefault(user => user.email == email);
+                u.password = util.Encryption.Hash.CreateHash(password);
+                db.SubmitChanges();
+            }
+        }
+
+        internal static user Get(string email) {
+            using (var db = new DataClassesDataContext()) {
+                return db.users.SingleOrDefault(u => u.email == email);
+            }
+        }
+
+        internal static user Get(string email, string password) {
+            using (var db = new DataClassesDataContext()) {
+                user user = db.users.SingleOrDefault(u => u.email == email);
+                if (user != null && util.Encryption.Hash.ValidatePassword(password, user.password)) {
+                    return user;
+                } else {
+                    return null;
+                }
+            }
+        }
+    }
+}
