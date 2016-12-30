10dc1e406f4e57df4d6b7875f7368112ecff8275
Haroen Viaene <haroen.viaene@student.odisee.be>
2016-12-30 16:23:46 +0100

    Add controllers for parkplan
    no views yet :help:

--- EuroSpaceCenter/Controllers/PlanController.cs
+++ EuroSpaceCenter/Controllers/PlanController.cs
@@ -1,21 +1,43 @@
 ﻿using EuroSpaceCenter.Models;
+using EuroSpaceCenter.util;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace EuroSpaceCenter.Controllers {
     [Authorize]
     public class PlanController : Controller {
-        // GET: Plan
+
         public ActionResult Index() {
-            return View();
+            IEnumerable<users_has_parkplan> all = parkplan.GetAll(user.Get(User.Identity.Name).id);
+            var accepted = all.Where(uhp => uhp.accepted).Select(uhp => uhp.parkplan);
+            ViewBag.UnAccepted = all.Where(uhp => !uhp.accepted).Select(uhp => uhp.parkplan);
+            return View(accepted);
+        }
+
+        public ActionResult Detail(int id) {
+            return View(parkplan.Get(parkplan_id: id));
         }
 
         [HttpPost]
         public ActionResult Create(parkplan plan) {
-            return View();
+            if (TryValidateModel(plan)) {
+                var p = plan.Create(user.Get(User.Identity.Name).id);
+                return RedirectToAction("Detail", p.id);
+            }
+            return View(plan);
         }
 
-        public ActionResult Join() {
-            return View();
+        public ActionResult Invite(int id, int user_id) {
+            if (parkplan.HasUser(user_id: id, parkplan_id: id)) {
+                parkplan.Invite(users_id: user_id, parkplan_id: id);
+                Flash.Set(TempData, "Invited");
+                return RedirectToAction("Detail", id);
+            } else {
+                Flash.Set(TempData, "That's not your plan 💁");
+                return RedirectToAction("Index");
+            }
+
         }
 
         [HttpPost]
---
+++ EuroSpaceCenter/Models/parkplan.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Web;
+
+namespace EuroSpaceCenter.Models {
+    public partial class parkplan {
+        internal parkplan Create(int users_id) {
+            using (var db = new DataClassesDataContext()) {
+                db.parkplans.InsertOnSubmit(this);
+                db.SubmitChanges();
+                db.users_has_parkplans.InsertOnSubmit(new users_has_parkplan() { users_id = users_id, parkplans_id = this.id, accepted = true });
+                db.SubmitChanges();
+                return this;
+            }
+        }
+
+        internal static parkplan Get(int parkplan_id) {
+            using (var db = new DataClassesDataContext()) {
+                return db.parkplans.FirstOrDefault(p => p.id == parkplan_id);
+            }
+        }
+
+        internal static IEnumerable<users_has_parkplan> GetAll(int user_id) {
+            using (var db = new DataClassesDataContext()) {
+                DataLoadOptions options = new DataLoadOptions();
+                options.LoadWith<users_has_parkplan>(t => t.parkplan);
+                db.LoadOptions = options;
+
+                return db.users_has_parkplans.Where(uhp => uhp.users_id == user_id);
+            }
+        }
+
+        internal static void Invite(int users_id, int parkplan_id) {
+            using (var db = new DataClassesDataContext()) {
+                db.users_has_parkplans.InsertOnSubmit(new users_has_parkplan() { users_id = users_id, parkplans_id = parkplan_id, accepted = false });
+                db.SubmitChanges();
+            }
+        }
+
+        internal static bool Accept(int user_id, int parkplan_id) {
+            using (var db = new DataClassesDataContext()) {
+                var has = db.users_has_parkplans.FirstOrDefault(uhp => uhp.parkplans_id == parkplan_id && uhp.users_id == user_id);
+                if (has != null) {
+                    has.accepted = true;
+                    db.SubmitChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        internal static bool HasUser(int user_id, int parkplan_id) {
+            using (var db = new DataClassesDataContext()) {
+                return db.users_has_parkplans.Any(uhp => uhp.parkplans_id == parkplan_id && uhp.users_id == user_id);
+            }
+        }
+    }
+}
