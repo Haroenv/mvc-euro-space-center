925b0fe30dea3dd9a9b887cc774c58f0fb3cef01
Haroen Viaene <hello@haroen.me>
2017-01-01 21:33:11 +0100

    change wording

--- EuroSpaceCenter/Controllers/HomeController.cs
+++ EuroSpaceCenter/Controllers/HomeController.cs
@@ -6,10 +6,6 @@ namespace EuroSpaceCenter.Controllers {
             return View();
         }
 
-        public ActionResult About() {
-            return View();
-        }
-
         public ActionResult Contact() {
             return View();
         }
